2924f1e3c5cd502f645f2c815a0c14a7338b15fe
aeinrw <aeinrw@gmail.com>
2020-01-09 13:13:08 +0800

    完成了EigenSystem函数

--- AdNDP/Input.py
+++ AdNDP/Input.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class AdNDP:
 
     # 宏变量
@@ -25,7 +26,7 @@ class AdNDP:
     # main函数变量
     MnSrch = 0
     NBOOcc = np.zeros(shape=__PNMax, dtype=np.float64)
-    NBOVec= np.zeros(shape=(__PBSz, __PNMax), dtype=np.float64)
+    NBOVec = np.zeros(shape=(__PBSz, __PNMax), dtype=np.float64)
     DResid = 0.0
     NBOCtr = np.zeros(shape=(__PNAt, __PNMax), dtype=np.int32)
     NBOAmnt = 0
@@ -169,7 +170,7 @@ class AdNDP:
                     continue
 
                 if (self.MnSrch == 0):
-                    #Subsets()
+                    # Subsets()
                     pass
                 print("NCtr={0:3d},AtBlQnt={1:6d}".format(NCtr, AtBlQnt))
                 PP = 0
@@ -178,9 +179,9 @@ class AdNDP:
                 while (1):
                     for k in range(AtBlQnt):
                         CBl[:NCtr] = AtBl[k][:NCtr]
-                        #BlockDMNAO()
-                        #EigenSystem()
-                        #EigenSrt()
+                        # BlockDMNAO()
+                        # EigenSystem()
+                        # EigenSrt()
 
                         vmax = np.max(EiVal)
                         imax = np.argmax(EiVal)
@@ -201,21 +202,21 @@ class AdNDP:
                         AtBlQnt = PP
                         print("***AtBlQnt:", AtBlQnt)
 
-                        #SortPrel()
-                        #DepleteDMNAO()
-                        DResid=self.__DMNAO[:self.BSz][:self.BSz].trace()
+                        # SortPrel()
+                        # DepleteDMNAO()
+                        DResid = self.__DMNAO[:self.BSz][:self.BSz].trace()
                         IndS = IndF
                         self.NBOAmnt = IndF
-                        print("PP={:4d},Indf={:4d},IndF={:4d},NBOAmnt={:4d}".format(PP, IndS, IndF, self.NBOAmnt))
+                        print("PP={:4d},Indf={:4d},IndF={:4d},NBOAmnt={:4d}".format(
+                            PP, IndS, IndF, self.NBOAmnt))
                         PP = 0
                     else:
                         break
                     if Cnt > 20:
                         break
 
-
     # IndF应用型!!
-    def __SortPrel(self,PP,PrelOcc,PrelCtr,NCtr,IndF,NBOOcc,NBOVec,PrelVec,NBOCtr):
+    def __SortPrel(self, PP, PrelOcc, PrelCtr, NCtr, IndF, NBOOcc, NBOVec, PrelVec, NBOCtr):
 
         ThrOcc = 0.0
         Cnt1 = 0
@@ -232,7 +233,7 @@ class AdNDP:
                 print(" ***ThrOcc= {:.5f}".format(ThrOcc))
             elif (int)(PrelOcc[i] * 100000) == (int)(ThrOcc * 100000):
                 PrelAccpt[Cnt1] = i
-                Cnt1+=1
+                Cnt1 += 1
                 print("A Ctr= ", PrelCtr[:NCtr][i] + 1)
                 print(" ***ThrOcc= {:.5f}".format(ThrOcc))
 
@@ -244,15 +245,15 @@ class AdNDP:
             NBOCtr[NCtr][IndF] = -1
             IndF += 1
 
-
     def DepleteDMNAO(self, NBOOcc, NBOVec, IndS, IndF):
         print("DeleteDMNAO OK")
-        for l in range(IndS,IndF,1):
+        for l in range(IndS, IndF, 1):
             for i in range(self.BSz):
                 for i in range(self.BSz):
-                    self.__DMNAO[i][j] -= NBOOcc[l] * NBOVec[i][l] * NBOVec[j][l]
+                    self.__DMNAO[i][j] -= NBOOcc[l] * \
+                        NBOVec[i][l] * NBOVec[j][l]
 
-    def BlockDMNAO(self, CBl, NCtr,DUMMY):
+    def BlockDMNAO(self, CBl, NCtr, DUMMY):
         flag = np.zeros(shape=self.__PBSz, dtype=np.bool)
         for l in range(NCtr):
             n1 = self.__AtBsRng[CBl[l]][0]
@@ -268,38 +269,104 @@ class AdNDP:
                     DUMMY[i][j] = 0.0
 
     def EigenSystem(self, DUMMY, EiVal, EiVec):
-        #n=BSz
+        # n=BSz
+        b = np.zeros(shape=self.__PBSz, dtype=np.float64)
+        z = np.zeros(shape=self.__PBSz, dtype=np.float64)
+
+        b[:self.BSz] = np.array([DUMMY[i][i] for i in range(self.BSz)])
+        EiVal[:self.BSz] = b[:self.BSz]
+
+        sm = 0.0
+        for i in range(1, 101):
+            sm = np.sum(np.triu(np.fabs(DUMMY[:self.BSz,:self.BSz]), 1))
+            assert sm != 0.0, "sm = 0.0"
+
+            print("EigenSystem: loop= {:4d},sum= {:lf}".format(i, sm))
+
+            if i < 4:
+                tresh = 0.2 * sm / (self.BSz ** 2)
+            else
+                tresh = 0.0
+
+            for ip in range(self.BSz - 1):
+                for iq in range(ip + 1: self.BSz):
+                    g = 100.0 * fabs(a[ip][iq])
+                    if i > 4 and np.fabs(EiVal[ip]) + g == np.fabs(EiVal[ip])) and np.fabs(EiVal[iq]) + g == fabs(EiVal[iq]):
+                        DUMMY[ip][iq] = 0.0
+                    elif np.fabs(DUMMY[ip][iq] > tresh):
+                        h = EiVal[iq] - EiVal[ip]
+                        if np.fabs(h) + g == np.fabs(h):
+                            t = a[ip][iq] / h
+                        else:
+                            theta = 0.5 * h / a[ip][iq]
+                            t = 1.0 / (np.fabs(theta) + np.sqrt(1.0+ theta ** 2))
+                            if theta < 0.0:
+                                t = -t
+
+                        c = 1.0 / np.sqrt(1 + t ** 2)
+                        s = t * c
+                        tau = s / (1.0+ c)
+                        h = t * DUMMY[ip][iq]
+                        z[ip] = z[ip] - h
+                        z[iq] = z[iq] + h
+                        EiVal[ip] = EiVal[ip] - h
+                        EiVal[iq] = EiVal[iq] + h
+                        DUMMY[ip][iq] = 0.0
+
+                        for j in range(ip):
+                            g = DUMMY[j][ip]
+                            h = DUMMY[j][iq]
+                            DUMMY[j][ip] = g - s * (h + g * tau)
+                            DUMMY[j][iq] = h + s * (g - h * tau)
+
+                        for j in range(ip+1,iq):
+                            g = DUMMY[ip][j]
+                            h = DUMMY[j][iq]
+                            DUMMY[ip][j] = g - s * (h + g * tau)
+                            DUMMY[j][iq] = h + s * (g - h * tau)
+
+                        for j in range(iq+1,self.BSz):
+                            g = DUMMY[ip][j]
+                            h = DUMMY[iq][j]
+                            DUMMY[ip][j] = g - s * (h + g * tau)
+                            DUMMY[iq][j] = h + s * (g - h * tau)
+
+                        for j in range(self.BSz):
+                            g = EiVec[j][ip]
+                            h = EiVec[j][iq]
+                            EiVec[j][ip] = g - s * (h + g * tau)
+                            EiVec[j][iq] = h + s * (g - h * tau)
+
+            for ip in range(self.BSz):
+                b[ip] = b[ip] + z[ip]
+                d[ip] = b[ip]
+                z[ip] = 0.0
+
+        print("\n****************\nToo many iterations in jacobi\n*************\n")
+
+
+
+
+
+
+
 
     def EigenSrt():
         pass
+    # GIT
 
     def Subsets():
         pass
+    # get
 
     def Output():
         pass
+    # git
 
     def BasisChange():
         pass
+        # git
 
     def NBOPlotMolden():
         pass
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # git
