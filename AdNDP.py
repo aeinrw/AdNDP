afe467c372c6cf2fe7af92bf984d91fadaf15b88
aeinrw <aeinrw@gmail.com>
2020-01-30 17:28:54 +0800

    重构项目,完成了全局变量和类的定义.

---
+++ AdNDP.py
@@ -0,0 +1,15 @@
+PNAt = 128
+PNVl = 9
+PNTot = 15
+PNMxa = 1024
+PBSz = 1024
+
+'''
+! NAt - number of atoms
+! NVal - number of valence electronic pairs
+! NTot - number of core and valence electronic pairs
+! NMax - the maximum number of NBO vectors to be found;
+         defines the size of NBOVec , NBOOcc , NMax
+! BSz - total number of basis functions;
+        defines the size of the density matrix
+'''
